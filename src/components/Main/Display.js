355a97ef6087ac6d0804ebd2f3da5a40838711e5
Kevin Smith <keveightysev@gmail.com>
2019-09-22 21:05:56 -0500

    Basic styles for portion

--- src/components/Main/Display.js
+++ src/components/Main/Display.js
@@ -1,30 +1,9 @@
 import React from 'react';
 
+import { displays } from './displays';
+
 const Display = () => {
-	return (
-		<pre>{`
-                 ...0...               
-             ....   |   ....           
-         ....       |       ....       
-     ....           |           ....   
- ....               |               ...
-0...                |               ...0
-|   ....            |           ....   |
-|       ....        |       ....       |
-|           ....    |   ....           |
-|               ....0...               |
-|                   |                  |
-|                   |                  |
-|                   |                  |
-|                   |                  |
-|                   |                  |
-......              |              .... 
-      .....         |         .....     
-           .....    |    .....          
-                ....|....               
-                    .     
-            `}</pre>
-	);
+	return <pre>{displays.defaultDisplay}</pre>;
 };
 
 export default Display;
--- src/components/Main/Room.js
+++ src/components/Main/Room.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Display from './Display';
 
 const Room = () => {
 	return (
-		<div>
-			<h2>A Generic Room</h2>
+		<RoomStyle>
+			<h2>Welcome to</h2>
 			<Display />
-			<p>An uneventful room with nothing in it.</p>
-		</div>
+			<p>Please enter your name.</p>
+		</RoomStyle>
 	);
 };
 
 export default Room;
+
+const RoomStyle = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	h2 {
+		font-size: 2.5rem;
+		margin-top: 30px;
+	}
+
+	pre {
+		width: 50%;
+	}
+
+	p {
+		margin-top: 30px;
+		font-size: 1.4rem;
+	}
+`;
