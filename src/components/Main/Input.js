1c144de9636aa29b472c65286f16618f8650850d
keveightysev <keveightysev@gmail.com>
2019-09-25 21:16:48 -0500

    Login and Register working (for test server

--- src/components/Main/Input.js
+++ src/components/Main/Input.js
@@ -1,119 +1,180 @@
-import React, { useState, useRef, useEffect, useContext } from 'react';
-import styled from 'styled-components';
+import React, { useState, useRef, useEffect, useContext } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
 
-import { Context } from '../../context';
+import { Context } from '../../context'
 
 const Input = () => {
-	const { dispatch } = useContext(Context);
-	const [input, setInput] = useState('');
-	const inputField = useRef(null);
-	useEffect(() => {
-		const tempEl = document.createElement('pre');
-		tempEl.style.fontSize = '1.8rem';
-		tempEl.style.display = 'inline';
-		tempEl.style.fontFamily = 'Arial';
-		tempEl.innerHTML = input;
-		document.body.appendChild(tempEl);
-		inputField.current.style.width = `${tempEl.offsetWidth + 10}px`;
-		tempEl.parentNode.removeChild(tempEl);
-	}, [input]);
+  const { dispatch } = useContext(Context)
+  const [input, setInput] = useState('')
+  const inputField = useRef(null)
+  useEffect(() => {
+    const tempEl = document.createElement('pre')
+    tempEl.style.fontSize = '1.8rem'
+    tempEl.style.display = 'inline'
+    tempEl.style.fontFamily = 'Arial'
+    tempEl.innerHTML = input
+    document.body.appendChild(tempEl)
+    inputField.current.style.width = `${tempEl.offsetWidth + 10}px`
+    tempEl.parentNode.removeChild(tempEl)
+  }, [input])
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		const value = document.forms[0][0].value;
-		if (!value) {
-			dispatch({ type: 'NO_INPUT' });
-			return;
-		}
-		const split = value.split(' ');
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const value = document.forms[0][0].value
+    if (!value) {
+      dispatch({ type: 'NO_INPUT' })
+      return
+    }
+    const split = value.split(' ')
 
-		switch (split[0].toLowerCase()) {
-			case 'login':
-				if (!split[1]) {
-					dispatch({
-						type: 'INCOMPLETE_COMMAND',
-						payload: 'Please enter your username and password',
-					});
-				} else if (split[1] && !split[2]) {
-					dispatch({
-						type: 'INCOMPLETE_COMMAND',
-						payload: 'Please enter your password',
-					});
-				} else {
-					setInput('');
-				}
-				return;
-			default:
-				dispatch({ type: 'INVALID_COMMAND' });
-				setInput('');
-				return;
-		}
-	};
+    try {
+      switch (split[0].toLowerCase()) {
+        case 'login':
+          if (!split[1]) {
+            dispatch({
+              type: 'INCOMPLETE_COMMAND',
+              payload: 'Please enter your username and password',
+            })
+          } else if (split[1] && !split[2]) {
+            dispatch({
+              type: 'INCOMPLETE_COMMAND',
+              payload: 'Please enter your password',
+            })
+          } else {
+            const res = await axios.post(
+              'https://lambda-mud-test.herokuapp.com/api/login/',
+              {
+                username: split[1],
+                password: split[2],
+              },
+            )
+            const { key } = res.data
+            localStorage.setItem('mudkey', key)
+            dispatch({ type: 'SET_KEY', payload: key })
+            const initRes = await axios.get(
+              'https://lambda-mud-test.herokuapp.com/api/adv/init/',
+              {
+                headers: {
+                  Authorization: `Token ${key}`,
+                },
+              },
+            )
+            console.log(initRes.data)
+            dispatch({ type: 'CHANGE_ROOM', payload: initRes.data })
+            setInput('')
+          }
+          return
+        case 'register':
+          if (!split[1]) {
+            dispatch({
+              type: 'INCOMPLETE_COMMAND',
+              payload: 'Please enter your username and password',
+            })
+          } else if (split[1] && !split[2]) {
+            dispatch({
+              type: 'INCOMPLETE_COMMAND',
+              payload: 'Please enter your password',
+            })
+          } else {
+            const res = await axios.post(
+              'https://lambda-mud-test.herokuapp.com/api/registration/',
+              {
+                username: split[1],
+                password1: split[2],
+                password2: split[2],
+              },
+            )
+            const { key } = res.data
+            localStorage.setItem('mudkey', key)
+            dispatch({ type: 'SET_KEY', payload: key })
+            const initRes = await axios.get(
+              'https://lambda-mud-test.herokuapp.com/api/adv/init/',
+              {
+                headers: {
+                  Authorization: `Token ${key}`,
+                },
+              },
+            )
+            console.log(initRes.data)
+            dispatch({ type: 'CHANGE_ROOM', payload: initRes.data })
+            setInput('')
+          }
+          return
+        default:
+          dispatch({ type: 'INVALID_COMMAND' })
+          setInput('')
+          return
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-	return (
-		<Form onSubmit={handleSubmit}>
-			<span>></span>
-			<div onClick={() => inputField.current.focus()}>
-				<input
-					type='text'
-					value={input}
-					onChange={e => setInput(e.target.value)}
-					ref={inputField}
-				/>
-				<Caret onClick={() => inputField.current.focus()}>▍</Caret>
-			</div>
-			<button type='submit'>send</button>
-		</Form>
-	);
-};
+  return (
+    <Form onSubmit={handleSubmit}>
+      <span>></span>
+      <div onClick={() => inputField.current.focus()}>
+        <input
+          type='text'
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          ref={inputField}
+        />
+        <Caret onClick={() => inputField.current.focus()}>▍</Caret>
+      </div>
+      <button type='submit'>send</button>
+    </Form>
+  )
+}
 
-export default Input;
+export default Input
 
 const Form = styled.form`
-	padding: 5px;
-	display: flex;
-	justify-content: space-between;
+  padding: 5px;
+  display: flex;
+  justify-content: space-between;
 
-	span {
-		font-size: 1.8rem;
-	}
-	div {
-		width: 100%;
-		display: flex;
-		align-content: center;
-		cursor: text;
+  span {
+    font-size: 1.8rem;
+  }
+  div {
+    width: 100%;
+    display: flex;
+    align-content: center;
+    cursor: text;
 
-		input {
-			padding-left: 5px;
-			border: none;
-			background: #000;
-			color: #fff;
-			font-size: 1.8rem;
-			caret-color: transparent;
-		}
-	}
+    input {
+      padding-left: 5px;
+      border: none;
+      background: #000;
+      color: #fff;
+      font-size: 1.8rem;
+      caret-color: transparent;
+    }
+  }
 
-	button {
-		border: none;
-		background: #000;
-		color: #fff;
-		font-size: 1.8rem;
-	}
-`;
+  button {
+    border: none;
+    background: #000;
+    color: #fff;
+    font-size: 1.8rem;
+  }
+`
 
 const Caret = styled.span`
-	font-sight: 2rem;
-	animation: blink 1s infinite;
+  font-sight: 2rem;
+  animation: blink 1s infinite;
 
-	@keyframes blink {
-		0% {
-			opacity: 0;
-		}
-		49% {
-			opacity: 0;
-		}
-		50% {
-			opacity: 1;
-		}
-	}
-`;
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    49% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+`
--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,43 @@
-import { displays } from './components/Main/Room/displays';
+import { displays } from './components/Main/Room/displays'
 
 export const initialState = {
-	heading: 'Welcome to',
-	display: displays.defaultDisplay,
-	text: 'Please enter a command.',
-	username: '',
-};
+  heading: 'Welcome to',
+  display: displays.defaultDisplay,
+  text: 'Please enter a command.',
+  username: '',
+}
 
 export const reducer = (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case 'NO_INPUT':
-			return { ...state, text: 'No command entered. Please enter a command.' };
-		case 'INVALID_COMMAND':
-			return {
-				...state,
-				text:
-					'The command entered is invalid. You can find a list of commands to the right of the screen.',
-			};
-		case 'INCOMPLETE_COMMAND':
-			return {
-				...state,
-				text: payload,
-			};
-		default:
-			return state;
-	}
-};
+  const { type, payload } = action
+  switch (type) {
+    case 'NO_INPUT':
+      return { ...state, text: 'No command entered. Please enter a command.' }
+    case 'INVALID_COMMAND':
+      return {
+        ...state,
+        text:
+          'The command entered is invalid. You can find a list of commands to the right of the screen.',
+      }
+    case 'INCOMPLETE_COMMAND':
+      return {
+        ...state,
+        text: payload,
+      }
+    case 'SET_KEY':
+      return {
+        ...state,
+        key: payload,
+      }
+    case 'CHANGE_ROOM':
+      const { title, description, name } = payload
+      console.log(title, description, name)
+      return {
+        ...state,
+        username: name,
+        heading: title,
+        text: description,
+      }
+    default:
+      return state
+  }
+}
